1d5501b754e12b11dfec93cd0dbf52554cd9cc40
Kyle Martin <kymartin@csumb.edu>
2020-11-25 00:34:40 -0800

    Added item string to ProductData

--- main.go
+++ main.go
@@ -37,38 +37,36 @@ func main() {
 	log.Fatal(svr.ListenAndServe())
 }
 
-func createSession(w http.ResponseWriter, r *http.Request) {
-	domain := "http//localhost:8080"
+func createSession(w http.ResponseWriter, req *http.Request) {
+	domain := "http://localhost:8080"
 	params := &stripe.CheckoutSessionParams{
 		PaymentMethodTypes: stripe.StringSlice([]string{
 			"card",
 		}),
 		LineItems: []*stripe.CheckoutSessionLineItemParams{
 			&stripe.CheckoutSessionLineItemParams{
-				PriceData: &stripe.String(string(stripe.CurrencyUSD)),
-				ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
-					Name: stripe.String("NVIDIA RTX 3090"),
+				PriceData: &stripe.CheckoutSessionLineItemPriceDataParams{
+					Currency: stripe.String(string(stripe.CurrencyUSD)),
+					ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
+						Name: stripe.String("NVIDIA RTX 3090"),
+					},
+					UnitAmount: stripe.Int64(2000),
 				},
 				Quantity: stripe.Int64(1),
 			},
 		},
 		Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)),
-		SuccessURL: stripe.String(domain + "success.html"),
-		CancelURL:  stripe.String(domain + "cancel.html"),
+		SuccessURL: stripe.String(domain + "/success.html"),
+		CancelURL:  stripe.String(domain + "/cancel.html"),
 	}
-
 	session, err := session.New(params)
-
 	if err != nil {
 		log.Printf("session.New: %v", err)
 	}
-
 	data := createCheckoutSessionResponse{
 		SessionID: session.ID,
 	}
-
 	js, _ := json.Marshal(data)
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-
 }
