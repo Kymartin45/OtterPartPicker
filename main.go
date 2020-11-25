64dc9aa58a908b516736a760d1818601f5773a13
Kyle Martin <kymartin@csumb.edu>
2020-11-25 00:22:40 -0800

    First commit to OtterPartPicker

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/checkout/session"
+)
+
+type createCheckoutSessionResponse struct {
+	SessionID string `json:"id"`
+}
+
+func main() {
+	stripe.Key = "sk_test_51Hpq4CIkuecL7QkUnVXbFdWYIskjwYpA8248lJ0sffdPBqslK7uzOScCG6RCXNhW3ZkziF7CJYEx65dVzAO9TZgf001jfrLSwH"
+
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "./static")
+	flag.Parse()
+	r := mux.NewRouter()
+
+	//Serve static html from localhost:8080/home
+	r.PathPrefix("/home/").Handler(http.StripPrefix("/home", http.FileServer(http.Dir(dir))))
+
+	svr := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(svr.ListenAndServe())
+}
+
+func createSession(w http.ResponseWriter, r *http.Request) {
+	domain := "http//localhost:8080"
+	params := &stripe.CheckoutSessionParams{
+		PaymentMethodTypes: stripe.StringSlice([]string{
+			"card",
+		}),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			&stripe.CheckoutSessionLineItemParams{
+				PriceData: &stripe.String(string(stripe.CurrencyUSD)),
+				ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
+					Name: stripe.String("NVIDIA RTX 3090"),
+				},
+				Quantity: stripe.Int64(1),
+			},
+		},
+		Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)),
+		SuccessURL: stripe.String(domain + "success.html"),
+		CancelURL:  stripe.String(domain + "cancel.html"),
+	}
+
+	session, err := session.New(params)
+
+	if err != nil {
+		log.Printf("session.New: %v", err)
+	}
+
+	data := createCheckoutSessionResponse{
+		SessionID: session.ID,
+	}
+
+	js, _ := json.Marshal(data)
+	w.Header().Set("Content-type", "application/json")
+	w.Write(js)
+
+}
