6250a913575c7f704a50859763fc9846ce01c947
Kyle Martin <kymartin@csumb.edu>
2020-11-30 23:24:51 -0800

    Changes to read/write timeouts

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ func main() {
 	svr := &http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:8080",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 20 * time.Second,
+		ReadTimeout:  20 * time.Second,
 	}
 
 	log.Fatal(svr.ListenAndServe())
